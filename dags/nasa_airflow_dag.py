447bb7dc859a4e46fa1a0bc9e73982ab6e2e6efc
sahanajagadish <sahana.jagadish@galepartners.com>
2025-02-06 16:47:49 +0530

    nasa_api_transformation

---
+++ dags/nasa_airflow_dag.py
@@ -0,0 +1,62 @@
+import sys
+import os
+from airflow import DAG
+from airflow.decorators import task
+from airflow.utils.dates import days_ago
+import pandas as pd
+from dotenv import load_dotenv
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from utils.utils import fetch_nasa_data, process_nasa_data, store_in_db, export_to_csv
+
+
+load_dotenv()
+api_key=os.getenv('NASA_API_KEY')
+
+# DAG default arguments
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": days_ago(1),
+    "retries": 1,
+}
+
+with DAG(
+    dag_id="nasa_neo_pipeline",
+    default_args=default_args,
+    schedule_interval="@daily",
+    catchup=False,
+    tags=["nasa", "etl", "postgres"],
+):
+
+    @task
+    def fetch_data():
+        """Fetch a list of asteroids from the NASA API."""
+        api_key = os.getenv("NASA_API_KEY")
+        start_date = os.getenv("START_DATE")
+        end_date = os.getenv("END_DATE")
+        return fetch_nasa_data(api_key, start_date, end_date)
+
+    @task
+    def transform_data(data):
+        """Process and clean the fetched NASA data."""
+        return process_nasa_data(data)
+
+    @task
+    def store_data(df):
+        """Store transformed data in PostgreSQL."""
+        store_in_db(df)
+
+    @task
+    def export_data(df):
+        """Export stored data to a CSV file."""
+        # export_to_csv(df, "/opt/airflow/dags/nasa_neo_data.csv")
+        export_to_csv(df, os.getenv("EXPORT_FILE_PATH"))
+
+    # Task dependencies
+    raw_data = fetch_data()
+    cleaned_data = transform_data(raw_data)
+    store_data(cleaned_data)
+    export_data(cleaned_data)
